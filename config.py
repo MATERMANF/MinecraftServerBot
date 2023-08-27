e4ffb290593bfc95f130a8eda305fe1beb65dff2
MATERMANF <nathanmfullerton@gmail.com>
2023-08-27 23:20:10 +0000

    Added configuration file so I don't accidentally leak my Bot Token again

---
+++ config.py
@@ -0,0 +1,8 @@
+#The Bot token that your Discord bot will use
+token = "your token here"
+#Time delay for updating persistant messages. In minutes.
+time_delay = 5
+#Folder to get the embed images
+imgDir = "images/"
+#Put your owner ID here so you have admin access to bot
+owner_id = "-1"
